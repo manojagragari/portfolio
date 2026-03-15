214081eef80aa92bec3dab269df4688cf614bd33
manoj <manojagrahari7521@gmail.com>
2026-03-15 16:47:01 +0530

    fix: serve media files in production for uploaded images

--- backend/django-portfolio-api/portfolio_api/urls.py
+++ backend/django-portfolio-api/portfolio_api/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import JsonResponse
+from django.views.static import serve
 
 
 def health_check(request):
@@ -17,6 +18,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Render setup stores uploads in MEDIA_ROOT; expose /media/* in production.
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
